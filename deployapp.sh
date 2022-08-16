5397b0fd1f680bbbd099699dfe0914eb98534057
wenjian80 <wenjian80@gmail.com>
2022-08-16 13:58:18 +0800

    commit

--- deployapp.sh
+++ deployapp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/u01/Oracle/Middleware/Oracle_Home/oracle_common/common/bin/wlst.sh manageApplication.py -u ${WLS_UID} -p ${WLS_PWD} -a 10.0.0.8:7001 -n mytestapplication -f "/u01/tmp/opdemo.war" -t MS1
+/u01/Oracle/Middleware/Oracle_Home/oracle_common/common/bin/wlst.sh manageApplication.py -u ${WLS_UID} -p ${WLS_PWD} -a 10.0.0.8:7001 -n mytestapplication -f "/u01/devops/opdemo.war" -t MS1
